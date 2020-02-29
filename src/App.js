7d01f863223ddbe147514c530c46c9ee9d4acb15
Joshua <joshaw66@gmail.com>
2020-02-28 18:48:13 -0700

    Added state hooks, some dynamic values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+
 import "./App.css";
 import BottomRow from "./BottomRow";
 import React, {useState} from "react";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+
+  const [lionsTeam, setLionsTeam] = useState(0);
+  const [tigersTeam, setTigersTeam] = useState(0);
 
   return (
     <div className="container">
@@ -21,7 +24,7 @@ function App() {
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{tigersTeam}32</div>
           </div>
         </div>
         <BottomRow />
@@ -29,8 +32,11 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button 
+          className="homeButtons__touchdown"onClick={() => setLionsTeam(lionsTeam + 6)}>
+          </button>
+          <button 
+          className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown">Away Touchdown</button>
